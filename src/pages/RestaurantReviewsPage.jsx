9134990580c391accd826cf0f4a5a082520a7ff5
alexlinddk <99322793+alexlinddk@users.noreply.github.com>
2022-03-15 19:45:34 +0100

    Icons

--- src/pages/RestaurantReviewsPage.jsx
+++ src/pages/RestaurantReviewsPage.jsx
@@ -1,18 +1,18 @@
 import ReviewList from "../components/ReviewList";
 import { useParams } from "react-router";
 import { useState } from "react";
-import { 
-    IonBackButton, 
-    IonButtons, 
-    IonContent, 
-    IonHeader, 
-    IonLabel, 
-    IonList, 
-    IonListHeader, 
-    IonPage, 
-    IonTitle, 
-    IonToolbar, 
-    useIonViewWillEnter 
+import {
+    IonBackButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonLabel,
+    IonList,
+    IonListHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    useIonViewWillEnter
 } from "@ionic/react";
 import ReviewListItem from "../components/ReviewListItem";
 
@@ -38,33 +38,32 @@ const RestaurantReviewsPage = () => {
         setRestaurant(restaurantData);
     }
 
-
-
     useIonViewWillEnter(() => {
         getReviews();
         getRestaurant();
     });
+
     return (
         <IonPage>
-        <IonHeader translucent>
-          <IonToolbar>
-            <IonButtons slot="start">
-              <IonBackButton text="Back" defaultHref="/restaurants/:id"></IonBackButton>
-            </IonButtons>
-            <IonTitle>{restaurant?.name ? restaurant.name : "Unknown Restaurant"}</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <IonList>
-            <IonListHeader>
-              <IonLabel>{reviews.length ? "Restaurant Reviews" : "No reviews yet"}</IonLabel>
-            </IonListHeader>
-            {reviews.map(review => (
-              <ReviewListItem review={review} key={review.id} />
-            ))}
-          </IonList>
-        </IonContent>
-      </IonPage>
+            <IonHeader translucent>
+                <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonBackButton text="Back" defaultHref="/restaurants/:id"></IonBackButton>
+                    </IonButtons>
+                    <IonTitle>{restaurant.name}</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonList>
+                    <IonListHeader>
+                        <IonLabel>{reviews.length ? "Restaurant Reviews" : "No reviews yet"}</IonLabel>
+                    </IonListHeader>
+                    {reviews.map(review => (
+                        <ReviewListItem review={review} key={review.id} />
+                    ))}
+                </IonList>
+            </IonContent>
+        </IonPage>
     );
 }
 
