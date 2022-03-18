f51653d4ddbfc3af3f6f249394fbd47ed7472a90
alexlinddk <99322793+alexlinddk@users.noreply.github.com>
2022-03-18 11:15:32 +0100

    User reviews

--- src/components/ReviewListItem.jsx
+++ src/components/ReviewListItem.jsx
@@ -1,36 +1,51 @@
-import { 
-    IonAvatar, 
-    IonCard, 
-    IonCol, 
-    IonImg, 
-    IonRow, 
-    IonText 
+import {
+    IonAvatar,
+    IonCard,
+    IonCol,
+    IonImg,
+    IonRow,
+    IonText,
+    IonTitle
 } from "@ionic/react";
+import { getAuth } from "firebase/auth";
+import { get } from "firebase/database";
+import { useEffect, useState } from "react";
+import { getUserRef } from "../firebaseConfig";
 
 
 const ReviewListitem = ({ review }) => {
-  const defaultPic = 'https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0=';
-  
+    const [user, setUser] = useState({});
+
+    const auth = getAuth();
+
+    async function getUserDataOnce() {
+        const snapshot = await get(getUserRef(auth.currentUser.uid));
+        const userData = snapshot.val();
+
+        setUser({
+            uid: auth.currentUser.uid,
+            ...userData
+        });
+        return userData;
+
+    }
+
+    useEffect(() => {
+        getUserDataOnce();
+    }, [])
+
     return (
         <IonCard>
-            {/* <IonRow>
+            <IonRow>
                 <IonCol>
-                    <IonAvatar>
-                        <IonImg src={user ? review.user.image : defaultPic} />
-                    </IonAvatar>
+                    <IonTitle>{review.title ? review.title : 'Title'}</IonTitle>
                 </IonCol>
                 <IonCol>
-                    <IonText>{review.user.email ? review.user.email : 'Anonymous'}</IonText>
+                    <IonText style={{color: 'var(--ion-color-primary)'}}>{review.date ? review.date : '01/01/2001'}</IonText>
                 </IonCol>
-            </IonRow> */}
-            <IonRow>
-                <IonText>{review.title ? review.title : 'Title'}</IonText>
             </IonRow>
             <IonRow>
-                <IonText>{review.date ? review.date : '01/01/2001'}</IonText>
-            </IonRow>
-            <IonRow>
-                <IonText>{review.body ? review.body : 'Description'}</IonText>
+                    <IonText style={{margin: '25px', }}>{review.body ? review.body : 'Description'}</IonText>
             </IonRow>
         </IonCard>
     );
